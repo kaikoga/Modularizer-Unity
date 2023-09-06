139728b85b720dfa415e3686a6d2b89d27b6fff8
kaikoga <info@mail.kaikoga.net>
2023-09-07 00:36:28 +0900

    hack

--- Modularizer/Packages/net.kaikoga.modularizer/Runtime/Silksprite/Modularizer/ModularizerConfig.cs
+++ Modularizer/Packages/net.kaikoga.modularizer/Runtime/Silksprite/Modularizer/ModularizerConfig.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 
 namespace Silksprite.Modularizer
 {
+    [AddComponentMenu("Modularizer/Modularizer Config")]
     public class ModularizerConfig : MonoBehaviour
     {
         public string exportDirectory;
         public Transform avatarRoot;
         public Renderer bodyRenderer;
-        public RendererSet[] modules;
+        public RendererSet[] modules = {};
         public bool unpackPrefab;
         // public bool setupMA = true;
         
-        public Renderer[] selectedRenderers;
+        public Renderer[] selectedRenderers = {};
     }
 }

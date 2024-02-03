3978c0f10df0239182d7b80379bdccfae5d1fbca
kaikoga <info@mail.kaikoga.net>
2024-02-03 23:09:13 +0900

    add simple GC

--- Modularizer/Packages/net.kaikoga.modularizer/Editor/Silksprite/Modularizer/Processors/BaseProcessor.cs
+++ Modularizer/Packages/net.kaikoga.modularizer/Editor/Silksprite/Modularizer/Processors/BaseProcessor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 // using nadena.dev.modular_avatar.core;
@@ -38,6 +39,13 @@ namespace Silksprite.Modularizer.Processors
                     }
                 }
 
+#if UNITY_2022_2_OR_NEWER
+                for (var i = 0; i < 10; i++)
+                {
+                    if (!GC(modularObject)) break;
+                }
+#endif
+
                 var assetPath = BuildPrefabPath(definition.ExportPath, module.ModuleName);
                 ModularizerTools.EnsureDirectory(assetPath);
                 PrefabUtility.SaveAsPrefabAsset(modularObject, assetPath);
@@ -56,6 +64,125 @@ namespace Silksprite.Modularizer.Processors
             {
                 IgnoreEmpty(renderer);
             }
+        }
+
+        bool GC(GameObject modularObject)
+        {
+            var references = new HashSet<Transform>();
+            var componentReferences = new HashSet<Component>();
+
+            foreach (var child in modularObject.GetComponentsInChildren<Transform>())
+            {
+                var components = child.GetComponents<Component>()
+                    .Where(c=> c is not Transform)
+                    .ToArray();
+                
+                foreach (var component in components)
+                {
+                    var isVrcPhysBone = false;
+                    // prefilter components
+                    switch (component.GetType().FullName)
+                    {
+                        case "VRC.SDK3.Dynamics.PhysBone.Components.VRCPhysBone":
+                            isVrcPhysBone = true;
+                            if (child.childCount == 0) continue;
+                            break;
+                        case "VRC.SDK3.Dynamics.PhysBone.Components.VRCPhysBoneCollider":
+                            // require reference
+                            continue;
+                    }
+
+                    // register components
+                    references.Add(component.transform);
+                    componentReferences.Add(component);
+
+                    // prefilter references
+                    if (component is SkinnedMeshRenderer smr)
+                    {
+                        var bones = smr.bones;
+                        var boneIsEffective = new bool[bones.Length];
+                        foreach (var bw in smr.sharedMesh.GetAllBoneWeights().Where(bw => bw.weight > 0f))
+                        {
+                            boneIsEffective[bw.boneIndex] = true;
+                        }
+                        for (var i = 0; i < bones.Length; i++)
+                        {
+                            var bone = bones[i];
+                            if (boneIsEffective[i])
+                            {
+                                references.Add(bone);
+                            }
+                            else
+                            {
+                                bones[i] = smr.rootBone;
+                            }
+                        }
+                        smr.bones = bones;
+                    }
+
+                    // register references
+                    var serialized = new SerializedObject(component);
+                    var it = serialized.GetIterator();
+                    while (true)
+                    {
+                        Object obj = null;
+                        switch (it.propertyType)
+                        {
+                            case SerializedPropertyType.ObjectReference:
+                                obj = it.objectReferenceValue;
+                                break;
+                            case SerializedPropertyType.ExposedReference:
+                                obj = it.exposedReferenceValue; 
+                                break;
+                        }
+
+                        switch (it.name)
+                        {
+                            case "ignoreTransforms":
+                                if (isVrcPhysBone) it.Next(false);
+                                continue;
+                        }
+
+                        switch (obj)
+                        {
+                            case GameObject gameObjectRef:
+                                references.Add(gameObjectRef.transform);
+                                break;
+                            case Component componentRef:
+                                references.Add(componentRef.transform);
+                                componentReferences.Add(componentRef);
+                                break;
+                        }
+
+                        if (!it.Next(true)) break;
+                    }
+                }
+            }
+
+            var hierarchicalReferences = references
+                .SelectMany(reference => reference.GetComponentsInParent<Transform>())
+                .Distinct().ToArray();
+            var gameObjectsToDestroy = modularObject.GetComponentsInChildren<Transform>()
+                .Except(hierarchicalReferences)
+                .Select(transform => transform.gameObject)
+                .OrderByDescending(o => o.GetComponentsInParent<Transform>().Length)
+                .ToArray();
+
+            var componentsToDestroy = modularObject.GetComponentsInChildren<Component>()
+                .Where(component => component is not Transform)
+                .Except(componentReferences)
+                .ToArray();
+                
+            foreach (var component in componentsToDestroy)
+            {
+                Object.DestroyImmediate(component);
+            }
+            foreach (var gameObject in gameObjectsToDestroy)
+            {
+                Object.DestroyImmediate(gameObject);
+            }
+
+            return gameObjectsToDestroy.Length > 0;
         }
 
         protected abstract GameObject InstantiateModule(GameObject gameObject);
